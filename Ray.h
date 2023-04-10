40526b43ea7b19799a847612c58ad2f61b29893f
Jan <jpetko1@gmail.com>
2023-04-10 21:39:00 +0200

    Added Ray class.

---
+++ Ray.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "vec3d.h"
+
+class Ray {
+ public:
+  explicit Ray(vec3d const& origin, vec3d const& dir)
+      : origin_(origin), dir_(dir) {}
+
+  vec3d origin() const { return origin_; }
+
+  vec3d direction() const { return dir_; }
+
+  vec3d at(double t) const { return origin_ + t * dir_; }
+
+ private:
+  vec3d origin_;
+  vec3d dir_;
+};
